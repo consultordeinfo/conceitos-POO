77a5a018e421d899c8f510ce265f4043f1c018fe
consultordeinfo <consultordeinfo@gmail.com>
2021-07-04 19:01:11 -0300

    Criação de nova classe.

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,20 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+class Conta ( //Tipo de construtor PRIMÁRIO
+    val agencia: String,
+    val numero:String,
+    val saldo: BigDecimal) {
+
+    //Funcionalidades da classe
+    //Função deposito
+    fun deposito (valor: BigDecimal) {
+
+    }
+
+    //Função saque
+    fun saque (valor: BigDecimal) {
+
+    }
+}
