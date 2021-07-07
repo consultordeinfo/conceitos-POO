791d586246039610e00b09d1b7852e231d4aedf4
consultordeinfo <consultordeinfo@gmail.com>
2021-07-07 20:56:04 -0300

    Modificações na classe Enum

--- src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteClienteTipoEnum.kt
@@ -3,7 +3,14 @@ package one.digitalinnovation.digionebank.testes
 import one.digitalinnovation.digionebank.ClienteTipo
 
 fun main() {
+    //foreach dentro da classe ClienteTipo usando Enum
     ClienteTipo.values().forEach { elemento ->
-        println("${elemento.name} - $elemento")
+        println("${elemento.name} - ${elemento.descricao}")
     }
+
+    val pf = ClienteTipo.PF
+    println("Da classe Enum ${pf.name} - ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println("Da classe Enum ${pj.name} - ${pj.descricao}")
 }
