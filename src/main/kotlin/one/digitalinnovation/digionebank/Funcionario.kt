bd0a5dec87873eb78ac29d529e96694a5cfb21b3
consultordeinfo <consultordeinfo@gmail.com>
2021-07-07 22:29:50 -0300

    Classe que herdão funcionário, com implemtação novas.

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+abstract class Funcionario (
+     nome: String,
+     cpf: String,
+    val salario: Double
+) : Pessoa (nome, cpf) {
+    protected abstract fun calculaAuxilio (): Double
+
+    override fun toString(): String = """
+        Nome: $nome
+        Cpf: $cpf
+        Salario: $salario
+        Auxilio: ${calculaAuxilio()}
+    """.trimIndent()
+}
